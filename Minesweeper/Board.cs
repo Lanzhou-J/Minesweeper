b07c3690fac0d3fbd57cb5a1275bdb5d5c945452
Lanzhou Jiang <lzjiang99@gmail.com>
2020-12-11 13:28:39 +1100

    deleted Square class

--- Minesweeper/Board.cs
+++ Minesweeper/Board.cs
@@ -2,12 +2,22 @@ namespace Minesweeper
 {
     public class Board
     {
+        private SquareState _empty = SquareState.Empty;
+        private SquareState _mine = SquareState.Mine;
         public Board(int size)
         {
             Size = size;
+            Squares = SetSquares();
         }
 
-        public Square[][] Squares { get; private set; }
+        private SquareState[][] SetSquares()
+        {
+            return new []{
+                new[]{_empty}
+            };
+        }
+
+        public SquareState[][] Squares { get; private set; }
         public int Size { get; private set; }
 
         public static Board CreateBoard( int size)
--- Minesweeper/Square.cs
+++
@@ -1,7 +0,0 @@
-namespace Minesweeper
-{
-    public class Square
-    {
-        
-    }
-}
