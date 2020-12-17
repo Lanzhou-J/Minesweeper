a694c06619d54ca218c8ccfffd23680792853437
Lanzhou Jiang <lzjiang99@gmail.com>
2020-12-17 16:02:21 +1100

    refactored Location class tests, all passed

--- MinesweeperTests/LocationTest.cs
+++ MinesweeperTests/LocationTest.cs
@@ -18,7 +18,7 @@ namespace MinesweeperTests
         public void GetNeighboursLocationsShould_ReturnEmptyList_ThereIsOnlyOneItemInLocationsList()
         {
             var location = new Location(0,0);
-            var locations = new List<Location>(){new Location(0, 0)};
+            var locations = new List<Location>(){location};
             var result = location.GetNeighboursLocations(locations);
             Assert.Null(result);
         }
@@ -26,23 +26,46 @@ namespace MinesweeperTests
         [Fact]
         public void GetNeighboursLocationsShould_ReturnAListOfOneLocation_WhenThereAre2LocationsInList()
         {
+            var left = new Location(0,0);
+            var right = new Location(0,1);
             
-            var locations = new List<Location>(){new Location(0, 0), new Location(0, 1)};
+            var locations = new List<Location>(){left, right};
             
-            var location = new Location(0,0);
-            var result = location.GetNeighboursLocations(locations);
-            Assert.NotEmpty(result);
-            Assert.Equal(0, result[0].X);
-            Assert.Equal(1, result[0].Y);
-            
-            var secondLocation = new Location(0,1);
-            var secondResult = secondLocation.GetNeighboursLocations(locations);
-            Assert.NotEmpty(secondResult);
-            Assert.Equal(0, secondResult[0].X);
-            Assert.Equal(0, secondResult[0].Y);
+            var leftNeighbours = left.GetNeighboursLocations(locations);
+            Assert.Single(leftNeighbours);
+            Assert.Contains(right, leftNeighbours);
+
+            var rightNeighbours = right.GetNeighboursLocations(locations);
+            Assert.Single(rightNeighbours);
+            Assert.Contains(left, rightNeighbours);
         }
         
+        [Fact]
+        public void GetNeighboursLocationsShould_ReturnAListO2Locations_WhenThereAre3LocationsInList()
+        {
+            var left = new Location(0,0);
+            var middle = new Location(0,1);
+            var right = new Location(0,2);
+            var locations = new List<Location>(){left, middle, right};
+            
+            var leftNeighbours = left.GetNeighboursLocations(locations);
+            Assert.Equal(2, leftNeighbours.Count);
+            Assert.Contains(middle, leftNeighbours);
+            Assert.Contains(right, leftNeighbours);
+        }
         
-
+        [Fact]
+        public void GetNeighboursLocationsShould_ReturnAListO3Locations_WhenThereAre4LocationsInList()
+        {
+            var topLeft = new Location(0,0);
+            var topRight = new Location(0,1);
+            var bottomLeft = new Location(1,0);
+            var bottomRight = new Location(1,1);
+            
+            var locations = new List<Location>(){topLeft, topRight, bottomLeft, bottomRight};
+            
+            var result = topLeft.GetNeighboursLocations(locations);
+            Assert.Equal(3, result.Count);
+        }
     }
 }
