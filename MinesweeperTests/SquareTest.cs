3b04a86996dd058b1147640e0688d6833cc58a9a
Lanzhou Jiang <lzjiang99@gmail.com>
2021-01-03 13:58:31 +1100

    add a test in Square test when square is not mine and is not revealed, passed

--- MinesweeperTests/SquareTest.cs
+++ MinesweeperTests/SquareTest.cs
@@ -5,17 +5,28 @@ namespace MinesweeperTests
 {
     public class SquareTest
     {
+        
         [Fact]
-        public void ToStringShould_ReturnADotString_WhenSquareIsMineAndIsNotRevealed()
+        public void ToStringShould_ReturnADotString_WhenSquareIsNotMineAndIsNotRevealed()
         {
             var location = new Location(1,1);
             var square = new Square(location);
             var result = square.ToString();
             Assert.Equal(".", result);
         }
+
+        [Fact]
+        public void ToStringShould_ReturnADotString_WhenSquareIsMineAndIsNotRevealed()
+        {
+            var location = new Location(1,1);
+            var square = new Square(location);
+            square.SetMine();
+            var result = square.ToString();
+            Assert.Equal(".", result);
+        }
         
         [Fact]
-        public void ToStringShould_ReturnAStarString_WhenIsMineAndIsRevealed()
+        public void ToStringShould_ReturnAStarString_WhenSquareIsMineAndIsRevealed()
         {
             var location = new Location(1,1);
             var square = new Square(location);
@@ -24,5 +35,6 @@ namespace MinesweeperTests
             var result = square.ToString();
             Assert.Equal("*", result);
         }
+        
     }
 }
