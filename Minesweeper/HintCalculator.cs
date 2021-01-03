ee8177713d984253edd59257fe11aac65f35fda3
Lanzhou Jiang <lzjiang99@gmail.com>
2021-01-03 15:53:36 +1100

    implemented SetHints function in HintCalculator, passed all tests

--- Minesweeper/HintCalculator.cs
+++ Minesweeper/HintCalculator.cs
@@ -1,9 +1,22 @@
+using System.Linq;
+
 namespace Minesweeper
 {
     public class HintCalculator
     {
         public void SetHints(Board board)
         {
+            foreach (var item in board.Squares)
+            {
+                if (board.Size < 2) continue;
+                if (board.Squares.Any(item=>item.IsMine))
+                {
+                    if (!item.IsMine)
+                    {
+                        item.SetValue(1);
+                    }
+                }
+            }
         }
     }
 }
