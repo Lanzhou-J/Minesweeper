cfd28973f580e83ea5c95ba865da6ca3e9ca2acf
Lanzhou Jiang <lzjiang99@gmail.com>
2021-01-03 13:50:12 +1100

    add a test case for ToString method in Square class, failed

--- MinesweeperTests/SquareTest.cs
+++ MinesweeperTests/SquareTest.cs
@@ -13,5 +13,15 @@ namespace MinesweeperTests
             var result = square.ToString();
             Assert.Equal(".", result);
         }
+        
+        [Fact]
+        public void ToStringShould_ReturnAStarString_WhenIsMineAndIsRevealed()
+        {
+            var location = new Location(1,1);
+            var square = new Square(location);
+            square.Reveal();
+            var result = square.ToString();
+            Assert.Equal("*", result);
+        }
     }
 }
