4a20f6353008e1ca3c36fe49f002ac2bc87033e6
Lanzhou Jiang <lzjiang99@gmail.com>
2021-01-05 16:35:35 +1100

    extract method in InputParser to improve readability

--- Minesweeper/InputOutput/InputParser.cs
+++ Minesweeper/InputOutput/InputParser.cs
@@ -8,13 +8,23 @@ namespace Minesweeper
        {
             const string delimiter = ",";
             var delimiterIndex = GetDelimiterIndex(input, delimiter);
-            var locationXInput = input.Substring(0, delimiterIndex);
-            var locationYInput = input.Substring(delimiterIndex+1);
+            var locationXInput = GetLocationXInputString(input, delimiterIndex);
+            var locationYInput = GetLocationYInputString(input, delimiterIndex);
             var locationXValue = int.Parse(locationXInput);
             var locationYValue = int.Parse(locationYInput);
             var newLocation = new Location(locationXValue, locationYValue);
             return newLocation;
         }
+
+       private static string GetLocationYInputString(string input, int delimiterIndex)
+       {
+           return input.Substring(delimiterIndex+1);
+       }
+
+       private static string GetLocationXInputString(string input, int delimiterIndex)
+       {
+           return input.Substring(0, delimiterIndex);
+       }
 
        private static int GetDelimiterIndex(string input, string delimiter)
        {
