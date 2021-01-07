338af03719764d0e92a090fbacf175e412530233
Lanzhou Jiang <lzjiang99@gmail.com>
2021-01-07 11:50:27 +1100

    add 2 more test for Rule class, all passed

--- MinesweeperTests/RuleTest.cs
+++ MinesweeperTests/RuleTest.cs
@@ -22,6 +22,19 @@ namespace MinesweeperTests
         }
         
         [Fact]
+        public void IsWinningConditionShould_ReturnFalse_WhenOneHintsOnBoardIsRevealed()
+        {
+            var board = Board.CreateEmptyBoard(2);
+            var mineGenerator = new MockMinesGenerator();
+            mineGenerator.PlaceMines(2, board);
+            var bottomLeft = new Location(1,0);
+            var hintOne = board.GetSquare(bottomLeft);
+            hintOne.Reveal();
+            var result = Rule.IsWinningCondition(board);
+            Assert.False(result);
+        }
+        
+        [Fact]
         public void IsLosingConditionShould_ReturnTrue_WhenOneMineOnBoardAreRevealed()
         {
             var board = Board.CreateEmptyBoard(2);
@@ -32,6 +45,16 @@ namespace MinesweeperTests
             mineOne.Reveal();
             var result = Rule.IsLosingCondition(board);
             Assert.True(result);
+        }
+        
+        [Fact]
+        public void IsLosingConditionShould_ReturnFalse_WhenNoMineOnBoardIsRevealed()
+        {
+            var board = Board.CreateEmptyBoard(2);
+            var mineGenerator = new MockMinesGenerator();
+            mineGenerator.PlaceMines(2, board);
+            var result = Rule.IsLosingCondition(board);
+            Assert.False(result);
         }
     }
 }
